0edb354eec23a62e634dfaf36896e00443879cc7
josemp-git <josemp@hotmail.com>
2022-05-30 21:02:12 +0000

    first commit

--- presignS3objects.py
+++ presignS3objects.py
@@ -1,5 +1,4 @@
 import boto3
-
 def lambda_handler(event, context):
     s3Client = boto3.client("s3")
     snsClient = boto3.client("sns")
@@ -7,9 +6,6 @@ def lambda_handler(event, context):
     #¿Cuantos segundos dura disponible la liga?
     #2628000 segundos = 730 horas = 1 mes
     expiryTime = "2628000"
-    
-    #SNS Topic al cuál se va a mandar la liga de descarga, se ingresa el ARN del tópíco.
-    topic_arn ="arn:aws:sns:*REGION*:*ACCOUNT*:*TOPIC*"
     
     #Nombres del objeto y del bucket se toman del evento PUT
     bucket_name = event['Records'][0]['s3']['bucket']['name']
@@ -22,10 +18,14 @@ def lambda_handler(event, context):
         )
     #print("Esta es la url: " + url)
     
-    #Se manda el correo
+    #Parámetros para correo
     mail_message = "Esta es la url del reporte: " + url
     mail_subject = "Reporte listo"
-    mandar_correo = snsClient.publish(
+    #SNS Topic al cuál se va a mandar la liga de descarga, se ingresa el ARN del tópíco.
+    topic_arn ="arn:aws:sns:*REGION*:*ACCOUNT*:*TOPIC*"
+    
+    #Se manda el correo por medio de SNS
+    snsClient.publish(
         TopicArn=topic_arn,
         Message=mail_message,
         Subject=mail_subject,
